ec3468224533e58a06e02c8d8af93fd0d2ae14f0
Jowan <jowanwondergem@gmail.com>
2017-02-06 11:09:02 +0100

    German locale added

---
+++ ui/locale/locale-de.php
@@ -0,0 +1,221 @@
+
+	<?php
+	//COMMON INFO
+	
+	$_SESSION['id_lan'] 	=  4;
+	$_SESSION['lang'] 		= 'de';
+	
+	//HEADER
+	$l_header_company = 'Happy - group';
+	$l_header_buss_of = '';
+	$l_header_lang = 'Languages';
+	$l_header_networks = 'Networks';
+	$l_header_info_general = 'Common Info';
+	$l_header_info_partners = 'Partner\'s Info';
+	$l_header_info_terms = 'Terms';
+	$l_header_login_button = 'Login';
+	$l_header_login_client = 'login client';
+	$l_header_login_partner = 'login partner';
+	$l_header_login_partner_mlm = 'login Buss Consultant';
+	$l_header_search_by = 'Search by';
+	$l_header_search_on_map = 'Map';
+	$l_header_search_on_city = 'City';
+	$l_header_search_on_zone = 'Zone';
+	$l_header_search_on_activity = 'Activity';
+	$l_header_contact_mail ='Send email';
+	$l_header_contact_direct ='Send email from website';
+	
+	//POPUP
+	$l_popup_category_choice = "Search by Activity";
+	
+	//MENU
+	
+	$l_menu_home = 'HOME';
+	$l_menu_search = 'SEARCH';
+	$l_menu_aboutus = 'ABOUT US';
+	$l_menu_register = 'REGISTER';
+	$l_menu_flyers = 'FLYERS';
+	$l_menu_contacts = 'CONTACTS';
+	$l_menu_german_partners = 'GERMAN PARTNERS';
+	
+	// FOOTER
+	
+	$l_footer_title_common = "Common";
+	$l_footer_title_register = "Register";
+	$l_footer_title_search = "Search";
+	$l_footer_title_login = "Login";
+	$l_footer_title_info = "Info";
+	
+	$l_footer_home = "Home";
+	$l_footer_about = "About Us";
+	$l_footer_flyer = "Flyers";
+	$l_footer_contact = "Contacts";	
+	$l_card_bonus = "Bonus Card";
+	$l_card_holiday = "Holiday Card";
+	$l_card_party = "Party Card";
+	$l_footer_register_agent = "Agente Happy";
+	$l_footer_register_partner = "Parceiro";
+	$l_footer_register_client = "Client";
+	$l_footer_register_mlm_partner = "Bussiness Consultant";
+	$l_footer_map = "Map";
+	$l_footer_search_zone = "Zone";
+	$l_footer_search_activity = "Activity";
+	$l_footer_login_cliente = "Cliente";
+	$l_footer_login_partners = "Parceiro";
+	$l_footer_login_mlm_partners = "Bussiness Consultant";
+	
+	$l_footer_info_general = 'Common Info';
+	$l_footer_info_partners = 'Info Partners';
+	$l_footer_info_terms = 'Terms';
+	$l_footer_copyright = "All Rights Reserved to HappyGroup ";
+	
+	//HOME PAGE
+	$l_head_title = 'Home';
+	
+	
+	//LOGIN & PASSWORD RECOVERY
+	
+	$l_login_partner = 'Login Partner';
+	$l_login_password = 'Recover Password';
+	$l_login_back = 'Back To Login';
+	$l_login_back_home = 'Back to Homepage';
+	$l_login_form_email = 'Email';
+	$l_login_form_password = 'Password';
+	$l_login_forgot_password = 'Forgot your password?';
+	$l_login_button_login = "Login";
+	$l_login_button_send = "Send";
+	
+	// MAP
+	$l_map_title_areas = 'Zones';
+	$l_map_title_cities = 'Cities';
+	$l_map_message_no_cities = 'There are no cities yet!<br><a href="contact.php">Contact us to become member!</a>';
+	
+	//ABOUT US
+	$l_about_title = 'About Us';
+	
+	//REGISTER
+	$l_register_title = 'Register';
+	$l_register_intro = 'sdasdasdsd';
+	$l_register_duration = 'Duration';
+	$l_register_price = 'Price';
+	$l_register_lbl_card_type = "Type of card";
+	$l_register_lbl_n_agent = "Nº Agent";
+	$l_register_lbl_first_name = "First Name";
+	$l_register_lbl_last_name = "Last Name";
+	$l_register_lbl_date_of_birth = "Date of Birth";
+	$l_register_lbl_phone = "Telephone";
+	$l_register_lbl_m_phone = "Mobile Phone";
+	$l_register_lbl_email = "Email";
+	$l_register_lbl_address = "Address";
+	$l_register_lbl_country = "Country";
+	$l_register_btn_submit = "Register";
+	
+	//REGISTER MLM PARTNER
+	$l_register_mlm_title = 'Register as MLM Happy Partner';
+	$l_register_mlm_lbl_contract_type = 'Contract Type';
+	$l_register_mlm_lbl_sponsor = "Nr. Sponsor";
+	$l_register_mlm_lbl_first_name = 'First Name';
+	$l_register_mlm_lbl_last_name = 'Last Name';
+	$l_register_mlm_lbl_address = 'Address';
+	$l_register_mlm_lbl_country = 'Country';
+	$l_register_mlm_lbl_city = 'City';
+	$l_register_mlm_zip_code = 'Zip Code';
+	$l_register_mlm_identification = 'ID / Passport';
+	$l_register_mlm_lbl_date_of_birth = 'Birth Date';
+	$l_register_mlm_lbl_phone = 'Phone';
+	$l_register_mlm_lbl_m_phone = 'Cell Phone';
+	$l_register_mlm_lbl_email = 'Email';
+	$l_register_mlm_lbl_password = 'Password';
+	$l_register_mlm_lbl_password_confirm = 'Confirm Password';
+	
+	
+	
+	//CONTACT 
+	$l_contact_title = "Contacts";
+	$l_contact_form_title = "Get in Contact!";
+	$l_contact_form_email = "Email";
+	$l_contact_form_email_ex = "ex. john.doe@mail.com";
+	$l_contact_form_subject = "Subject";
+	$l_contact_form_subject_ex = "ex. More Information, about ...";
+	$l_contact_form_message = "Message";
+	$l_contact_form_message_ex = "ex. I would like to know more about...";
+	$l_contact_form_btn_sumbit = "Send";
+	$l_contact_form_btn_sending = "Sending...";
+	$l_contact_form_sent = "Message succesfully sent!";
+	$l_contact_form_sent_error = "Error while sending, try again";
+	$l_contact_view_map = "Enlarge Map";
+	
+	//Flyers
+	$l_flyers_title = 'Flyers';
+	$l_flyers_cities = 'Cities';
+	$l_flyers_zones = 'Zones';
+	$l_flyers_themes = 'Themes';
+	$l_flyers_no_flyers = 'None at the moment';
+	$l_flyers_intro = 'Here you will find all of our flyers with our discounts, divided under different categories! <br /> <br>These flyers can also be found in adherent places to Happygroup and tourism centers.';
+	
+	// PARTNERS
+	$l_partners_title = 'Partners';
+	$l_partners_in_zone = 'Partners in Zone';
+	$l_of_category = "of category";
+	$l_partners_per_page = "Partners on page";
+	$l_partners_no_results ="No results with these criteria";
+	$l_partners_go_back = "Try again";
+	$l_partners_view_website = "View Website";
+	$l_partners_next = "Next";
+	$l_partners_prev = "Previous";
+	$l_partners_search_again = "Search Again";
+	$l_partner_not_inserted = "Not inserted yet!";
+	
+	//POPUP
+	
+	$l_popup_activity_title = "Choose Activity";
+	$l_popup_login_title = "Do Login";
+	
+	//PARTNER
+	
+	$l_partner_title_happygroup ='Partner of Happy-Group';
+	$l_partner_title_discount = 'Discounts &amp; News ';
+	$l_partner_title_contacts = 'Contacts';
+	$l_partner_lbl_country = 'Country';
+	$l_partner_lbl_zip_code = 'Zip-code';
+	$l_partner_lbl_area = 'Region';
+	$l_partner_lbl_address = 'Address';
+	$l_partner_lbl_phone_1 = 'Phone 1';
+	$l_partner_lbl_phone_2 = 'Phone 2';
+	$l_partner_lbl_email = 'Email';
+	$l_partner_lbl_website= 'Website';
+	$l_link_back_happy = 'Back to Happy-Group';
+	$l_partner_contact_title = 'Send us a Message!';
+	$l_partner_contact_email = 'Email';
+	$l_partner_contact_email_placeholder = 'Write your email';
+	$l_partner_contact_subject = 'Subject';
+	$l_partner_contact_subject_placeholder = 'Write your subject';
+	$l_partner_contact_message = 'Message';
+	$l_partner_contact_message_placeholder = 'Write your message';
+	$l_partner_contact_btn_send = 'Send';
+	$l_partner_tab_info = "Information";
+	$l_partner_tab_contact = "Contacts";
+	$l_partner_go_back = "Back to";
+	
+	//EMAIL TEMPLATES
+	$l_template_title = 'Password Recovery';
+	$l_template_subtitle = 'Password is changed';
+	$l_template_text = 'Your new access details for your admin panel:<br><br>'.
+	'Email: XXXEMAIL<br>'.
+	'Password: XXXPASS';
+	
+	//EMAIL AUTORESPONDER
+	$l_template_autoresponder_title = 'Automatic Response';
+	$l_template_autoresponder_subtitle = 'Your message is delivered!';
+	$l_template_autoresponder_text = 'Thank you for contacting, we will get in contact within a couple of days. <br>
+	Happy-Group wishes you a pleasant day!';
+	
+	//EMAIL MESSAGES
+	$l_email_message_ok_1 = 'An email is sent to your email: ';
+	$l_email_message_ok_2 =' with a new password!, Check your mail within minutes';
+	$l_email_message_not_in_database ='Email is not is database!';
+	$l_email_message_not_sent ='Email is not sent!';
+	
+	
+	
+	?>
